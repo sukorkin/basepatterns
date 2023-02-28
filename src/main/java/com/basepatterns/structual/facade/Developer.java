d8ac48c5219941f672e75449f34082ecdcdd210d
SYKorkin <sergiy.korkyn@ionidea.com>
2023-02-28 14:09:47 +0300

    Facade

---
+++ src/main/java/com/basepatterns/structual/facade/Developer.java
@@ -0,0 +1,11 @@
+package com.basepatterns.structual.facade;
+
+public class Developer {
+    public void doJobBeforeDeadline(BugTracker bugTracker) {
+        if (bugTracker.isActiveSprint()) {
+            System.out.println("Developer is solving problem...");
+        } else {
+            System.out.println("Developer is reading Habrahabr...");
+        }
+    }
+}
---
+++ src/main/java/com/basepatterns/structual/facade/Job.java
@@ -0,0 +1,7 @@
+package com.basepatterns.structual.facade;
+
+public class Job {
+    public void doJob() {
+        System.out.println("Job in progress...");
+    }
+}
