56606918341fb35eab6e9fbc9c9964de5deecb4c
SYKorkin <sergiy.korkyn@ionidea.com>
2023-03-02 15:40:06 +0300

    Chain

---
+++ src/main/java/com/basepatterns/behavioral/chain/SMSNotifier.java
@@ -0,0 +1,13 @@
+package com.basepatterns.behavioral.chain;
+
+public class SMSNotifier extends Notifier{
+
+    public SMSNotifier(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Sending SMS to manager: " + message);
+    }
+}
